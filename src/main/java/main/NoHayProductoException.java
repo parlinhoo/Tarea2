4f6d0f721997d90c64316da7573f9386c5864ec7
Bastian <bastianceba@gmail.com>
2023-10-12 11:41:24 -0300

    feat: Se agregan nuevas excepciones

---
+++ src/main/java/main/NoHayProductoException.java
@@ -0,0 +1,5 @@
+public class NoHayProductoException extends Exception{
+    public NoHayProductoException(String errorMessage) {
+        super(errorMessage);
+    }
+}
---
+++ src/main/java/main/PagoIncorrectoException.java
@@ -0,0 +1,5 @@
+public class PagoIncorrectoException extends Exception{
+    public PagoIncorrectoException(String errorMessage){
+        super(errorMessage);
+    }
+}
