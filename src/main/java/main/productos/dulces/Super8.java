f5b8c35e677afdc832e88df41f4059e3486fd2d1
Bastian <bastianceba@gmail.com>
2023-10-16 23:50:46 -0300

    fix: Corrección de un error al retornar el String al consumir Super8

--- src/main/java/main/productos/dulces/Super8.java
+++ src/main/java/main/productos/dulces/Super8.java
@@ -2,6 +2,6 @@ package main.productos.dulces;
 
 public class Super8 extends Dulce{
     @Override
-    public String comsumir() {return null;}
+    public String comsumir() {return "super8";}
     public Super8(int serie) {super(serie);}
 }
