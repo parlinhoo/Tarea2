6d02c06efa28835158f0e6f72db069e0ae5abb15
Bastian <bastianceba@gmail.com>
2023-10-19 11:46:57 -0300

    Fusionar ambas ramas

--- src/main/java/main/monedas/Moneda.java
+++ src/main/java/main/monedas/Moneda.java
@@ -1,9 +1,29 @@
 package main.monedas;
 
+/**
+ *Monedas polimorficas que sirven como metodo de pago
+ * @author Bastiän Ceballos Z.
+ * @author Fabián González U.
+ */
+
 public abstract class Moneda implements Comparable<Moneda> {
+    /**
+     * Getter de la serie de la moneda
+     * @return Dirección en memoria de la moneda
+     */
     public Moneda getSerie() {return this;}
+
+    /**
+     * Getter del valor de la moneda
+     * @return Entero equivalente al valor de la moneda
+     */
     public abstract int getValor();
 
+    /**
+     * Compara 2 monedas segun su valor
+     * @param o moneda con la que comparar
+     * @return 0  si son iguales, -1 si "o" es mayor y 1 si "o" es menor
+     */
     @Override
     public int compareTo(Moneda o) {
         int ret = 0;
@@ -20,5 +40,6 @@ public abstract class Moneda implements Comparable<Moneda> {
         return String.format("$%d (Serie %s)", this.getValor(), Integer.toHexString(this.hashCode()));
     }
 
+    /** Constructor por defecto */
     public Moneda() {}
 }
